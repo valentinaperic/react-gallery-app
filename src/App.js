d410259edb568e9b278cd4ef5889dc0edcc97dc4
Valentina Peric <valentinaperic24@gmail.com>
2021-02-15 19:19:21 -0500

    moved api call back to app.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch, Redirect} from 'react-router-dom';
 import { useHistory } from "react-router-dom";
 import './App.css';
+import axios from 'axios';
+import apiKey from './config';
 
 import SearchForm from './components/SearchForm';
 import Nav from './components/Nav';
@@ -17,23 +19,40 @@ export default class App extends Component {
     }
   }
 
-  performSearch = (query) => {
-    console.log(`searchText: ${query}`);
-    this.setState({ searchText: query });
-    console.log(`searchText State: ${this.state.searchText}`);
-   
+  componentDidMount() {
+    this.searchPhotos();
   }
 
+  performSearch = async (query) => {
+    console.log(`searchText: ${query}`);
+    await this.setState({ searchText: query });
+    console.log(`searchText State: ${this.state.searchText}`);
+  }
+
+
+  searchPhotos = (query) => {
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
+        .then(response => {
+            this.setState({
+                photos: response.data.photos.photo
+            })
+        }) 
+        .catch(error => {
+            console.log('Error fetching and parsing data', error);
+        }
+    ); 
+  }
+
+
   render() { 
-    let text = this.state.searchText;
     return (
       <div className="container">
         <BrowserRouter>
           <SearchForm onSearch={this.performSearch} />
           <Nav />
-          <PhotoGallery data={text} />
+          <PhotoGallery data={this.state.photos} />
           <Route path="/search/:query" render={(props) => (
-            <PhotoGallery key={props.location.key} {...props} />
+            <PhotoGallery key={props.location.key} data={this.state.photos} />
           )} />
         </BrowserRouter>
       </div>
--- src/components/PhotoGallery.js
+++ src/components/PhotoGallery.js
@@ -1,58 +1,37 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Photo from './Photo';
-import axios from 'axios'
-import apiKey from '../config';
 
-export default class App extends Component {
 
-    constructor(props) {
-        super(props);
-        this.state = {
-          photos: []
-        }
+const PhotoGallery = props => {
+
+    let data;
+    if (props.data) {
+        data = props.data
+    } else {
+        data = props.match.params;
     }
 
-    componentDidMount() {
-        console.log(this.props);
-        //this.searchPhotos();
-    }
-
-
-    searchPhotos = () => {
-        const { query } = this.props.match.params;
-        axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
-            .then(response => {
-                this.setState({
-                    photos: response.data.photos.photo
-                })
-            }) 
-            .catch(error => {
-                console.log('Error fetching and parsing data', error);
-            }
-        ); 
-    }
-    
-    render() {
-        let photos;
-        if (this.state.photos.length > 0) {
-            photos = this.state.photos.map(element =>
-                <Photo 
-                    key={element.id}
-                    server={element.server} 
-                    id={element.id} 
-                    secret={element.secret} 
-                    title={element.title}
-                />
-            );
-        }
-
-        return (
-            <div className="photo-container">
-                <h2>Results</h2>
-                <ul>
-                    {photos}
-                </ul>
-            </div>
+    let photos;
+    if (data.length > 0) {
+        photos = data.map(element =>
+            <Photo 
+                key={element.id}
+                server={element.server} 
+                id={element.id} 
+                secret={element.secret} 
+                title={element.title}
+            />
         );
     }
-};
+
+    return (
+        <div className="photo-container">
+            <h2>Results</h2>
+            <ul>
+                {photos}
+            </ul>
+        </div>
+    );
+}
+
+export default PhotoGallery;
