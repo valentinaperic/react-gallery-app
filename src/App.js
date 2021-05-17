619c6de2e0e11cbc93ef8537f76b5a82bcfc3110
Valentina Peric <valentinaperic24@gmail.com>
2021-05-17 19:06:46 -0400

    removed an extra space

--- src/App.js
+++ src/App.js
@@ -34,7 +34,6 @@ export default class App extends Component {
    */
 
   searchPhotos = (query) => {
-
     axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${query}&per_page=24&format=json&nojsoncallback=1`)
     .then(response => {
       switch(query) {
